7787b8a5c84c09b2a8929d241923ddf8c2bb04bb
gnujesus <jotamartinezd@gmail.com>
2025-07-02 14:57:36 -0400

    added root finder

--- src/Helpers/IOHelper.php
+++ src/Helpers/IOHelper.php
@@ -2,10 +2,28 @@
 
 namespace Gnu\Scaffy\Core\Helpers;
 
+use RecursiveDirectoryIterator;
+
 class IOHelper
 {
-	static function findDirectory($dirName)
+
+	/*
+	* Return the root directory of the project. This is, wherever the composer.json exists.
+	*/
+	static function findRootDirectory(): string
 	{
-		$projectSrc = scandir('<project-src-path>');
+		$dir = __DIR__;
+
+		while (!file_exists($dir . "/composer.json")) {
+			$parent = dirname($dir);
+
+			if ($parent === $dir) {
+				throw new \RuntimeException("Could not find project root (composer.json not found)");
+			}
+
+			$dir = $parent;
+		}
+
+		return $dir . "/app";
 	}
 }
